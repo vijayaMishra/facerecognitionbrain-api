126ccfb29cf99174f6ce30f085326ee95e22fabb
Vijaya Mishra <vijayamishra1709@gmail.com>
2020-05-26 01:43:55 +0530

    modified server.js to connect to our front-end

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
 
 const database = {
     users: [
         {
             id: '123',
             name: 'John',
-            email: 'john@gmail.com',
             password: 'cookies',
+            email: 'john@gmail.com',
             entries: 0,
             joined: new Date()
         },
         {
             id: '124',
             name: 'Sally',
-            email: 'sally@gmail.com',
             password: 'bananas',
+            email: 'sally@gmail.com',
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", '$2a$10$oS16Vw6KvT9b3xSSL7E5Leg7pQf8y3gKJm.cPpCSsM1gjdj3aFkkq', function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", '$2a$10$oS16Vw6KvT9b3xSSL7E5Leg7pQf8y3gKJm.cPpCSsM1gjdj3aFkkq', function(err, res) {
+        console.log('second guess', res);
+    });
     if(req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
         res.json('signing')
@@ -40,6 +57,10 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     console.log(hash);
+    // });
     database.users.push({
         id: '125',
         name: name,
@@ -79,6 +100,16 @@ app.put('/image', (req, res) => {
         res.status(400).json('user not found');
     }
 })
+
+
+
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
